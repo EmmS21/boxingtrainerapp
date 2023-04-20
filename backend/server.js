fb23ede0828031d666a0f87e7d744f771c525952
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2023-04-20 14:06:32 -0500

    added firebase service account credentials to .env and creating session cookie to handle authentication when user logs in

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const cors = require('cors')
 const admin = require('firebase-admin');
-const serviceAccount = require('./boxingtrainer.json');
 const session = require('cookie-session');
+
 
 const app = express();
 const port = process.env.PORT || 8000
+
+require('dotenv').config();
+
+const serviceAccount= {
+    type : process.env.TYPE,
+    project_id : process.env.PROJECT_ID,
+    private_key_id : process.env.private_key_id,
+    private_key : process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email : process.env.CLIENT_EMAIL,
+    client_id : process.env.CLIENT_ID,
+    auth_uri : process.env.AUTH_URI,
+    token_uri : process.env.TOKEN_URI,
+    auth_provider_x509_cert_url : process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url : process.env.CLIENT_X509_CERT_URL
+}
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +31,9 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000
 }));
 
+app.get('/test', (req, res) => {
+    res.json({message: JSON.stringify(serviceAccount)});
+}); 
 
 app.post('/auth', async (req, res) => {
     const uid = req.body.id
@@ -31,7 +49,3 @@ app.post('/auth', async (req, res) => {
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
-
-// app.use(session({
-//     name: 'authSession', 
-// }))
